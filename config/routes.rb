eb9309a31af1235028c4f61aa54488582cd73286
Ivan Oliynyk <barjakall@gmail.com>
2024-03-06 22:58:14 +0000

    Worked on the mission board

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { invitations: "users/invitations" }
   root to: "pages#home"
 
-  resources :missions, only: [:index]
+  resources :missions, only: [:index, :new, :create] do
+    member do
+      get :complete
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
