b09b367697f5af5243f3fdde21ce5a591b4181d0
Ahmed Bouguacha <ahmad.bougacha@gmail.com>
2024-03-06 17:14:41 +0100

    Seeded chalenges

---
+++ db/couple_challenges_data.rb
@@ -0,0 +1,110 @@
+COUPLE_CHALLENGES =[
+  {
+    title: "Mistery ingredient",
+    details: "Ready to spice things up in the kitchen? Select a mystery, out-of-the-box, and hilarious ingredient for your partner to get creative with in the kitchen. Wrap it up in silly disguises or unveil it from a mysterious box for that extra thrill. Get set for a meal that's guaranteed to be unforgettable!",
+    difficulty: 2,
+    estimated_time: 3,
+    prompt_1: "Hey Chef Extraordinaire! Your mission, should you choose to accept it, is to select an ingredient that's not just funny but also a bit bizarre. Think outside the box! Let your creativity run wild, and let's see what kind of culinary chaos you can stir up!",
+    prompt_2: "Hey Kitchen Maestro! How about preparing a themed playlist to accompany your cooking adventure? Choose songs that match the vibe of the mystery ingredient you're planning to suprise your partner with and play it in the background as you cook together!",
+    exp: 20
+  },
+  {
+    title: "Creative Date Night",
+    details: "Welcome to the Pottery Sculpture Challenge: Couple's Edition! Get ready to put your artistic skills to the test as you attempt to create hilarious clay portraits of each other. But here's the twist: each sculpture must be accompanied by something your partner loves! Once your masterpieces are complete, it's time for the big reveal. Present your clay creations to each other and prepare for a funny gift exchange.",
+    difficulty: 3,
+    estimated_time: 3,
+    prompt_1: "Hey Rodin! While sculpting your partner's clay portrait, you must sneak in a tiny love message or inside joke, and hide it within the folds of your masterpiece. Include a playful instruction 'In case of emergency, break me open for a heartfelt surprise!",
+    prompt_2: "Hey Sculpting Savant! As you craft your partner's clay portrait, consider adding a detachable feature that allows them to engage with the piece in a unique and meaningful way. Think along the lines of interchangeable facial expressions or accessories that enable your partner to customize the sculpture. Get ready for a pottery session that's as interactive as it is inventive!",
+    exp: 30
+  },
+  {
+    title: "Bonjour, Hola, Ciao!",
+    details: "Get ready for a linguistic adventure! Each of you selects a language they want to learn for the challenge. Throughout the day, whenever possible, interact with each other using that language. You can send messages, leave notes or use learning apps to try to learn small phrases. At the end of the day, share a drink and and try to guess which language you where being speaken to.",
+    difficulty: 3,
+    estimated_time: 3,
+    prompt_1: "Here's a fun twist for your challenge: every time you use your chosen language, you must insert a fictional character's name or reference into the conversation. Whether it's a superhero or a literary figure, sneak them into your dialogue and see if, at the end of the day, your partner can catch the nods!",
+    prompt_2: "Here's a twist to spice up your challenge: every time you use your chosen language to communicate with your partner, you must incorporate one animal noise into the conversation. Whether it's a meow, a bark, or a quack, add a wild touch to your linguistic adventure! At the end, try to understand if your partner got that funny twist!",
+    exp: 30
+  },
+  {
+    title: "Lights, Camera, Adventure!",
+    details: "Plan an adventure to explore a nearby town. During the day, there will be plenty of photo opportunities! We've selected some iconic movie quotes for you to capture in your photos. Channel your inner movie stars and bring these famous lines to life: 'He's looking at you, kid.' - Casablanca; 'May the Force be with you.' - Star Wars; 'To infinity and beyond!' - Toy Story; 'You had me at hello.' - Jerry Maguire; 'Just keep swimming.' - Finding Nemo; 'Nobody puts Baby in a corner.' - Dirty Dancing. Get ready to strike a pose, recreate a scene, or find the perfect backdrop to capture the essence of each movie quote!",
+    difficulty: 3,
+    estimated_time: 3,
+    prompt_1: "Alongside capturing those iconic movie quotes themed photos, here's your unique task: incorporate, casually yet meaningfully, these 4 words into your conversations with your partner: 1) Serendipity; 2) Mellifluous; 3) Fandango; and 4) Quixotic. Whether you're describing the serendipitous discoveries you've made, discussing the mellifluous sounds of the city, or planning our next quixotic escapade, let these words add an extra layer of fun!",
+    prompt_2: "While capturing those iconic movie quotes themed photos, here's your unique task: incorporate, casually yet meaningfully, these 4 traditional sayings into your conversations with your partner: 1) 'Hit the hay.'; 2) 'Raining cats and dogs.'; 3) 'I'm on cloud nine.'; and 4) 'A bird in the hand is worth two in the bush.'.  Feel free to directly translate these into your native language while enjoying your partner's confused look when you apply them!",
+    exp: 30
+  },
+  {
+    title: "Guess Who's Smoochy!",
+    details: "Here's how 'Guess Who's Smoochy!' works: each of you will be presented with a series of questions about your loved one. Take your time to answer honestly. Once you've both completed the questions, it's time to compare your answers! Prepare to be amazed by how well you know each other... or perhaps discover some delightful surprises along the way!",
+    difficulty: 1,
+    estimated_time: 1,
+    prompt_1: "1. If your partner could have any superpower, what would it be and why?
+    2. If your partner could only eat one type of cuisine for the rest of their life, what cuisine would they choose?
+    3. If your partner could only listen to one song for the rest of their life, what would it be?
+    4. What's the most unexpected talent your partner has?
+    5. If you both got stranded on a deserted island, what would be your partner's priority?
+    6. If your partner won the lottery, what's the most ridiculous thing they would splurge on first?
+    7. What was your partner's childhood dream?
+    8. If your partner could time travel to any era, past or future, what would it be and what would he/she do?
+    9. If your partner could have any fictional character as a best friend, who would it be?
+    10. What was the most heartwarming moment you've shared with your partner?",
+    prompt_2: "1. If your partner could have any superpower, what would it be and why?
+    2. If your partner could only eat one type of cuisine for the rest of their life, what cuisine would they choose?
+    3. If your partner could only listen to one song for the rest of their life, what would it be?
+    4. What's the most unexpected talent your partner has?
+    5. If you both got stranded on a deserted island, what would be your partner's priority?
+    6. If your partner won the lottery, what's the most ridiculous thing they would splurge on first?
+    7. What was your partner's childhood dream?
+    8. If your partner could time travel to any era, past or future, what would it be and what would he/she do?
+    9. If your partner could have any fictional character as a best friend, who would it be?
+    10. What was the most heartwarming moment you've shared with your partner?",
+    exp: 10,
+  },
+  {
+    title: "Hollywood Hijinks",
+    details: "Get ready to step into the shoes of your partner's favorite movie character and bring them to life like never before. Each of you will choose your partner's favorite movie character to impersonate. Dress the part, learn their lines, and immerse yourself in their history. Then, on a special dinner date, surprise each other by embodying these characters and watch the magic unfold!",
+    difficulty: 2,
+    estimated_time: 3,
+    prompt_1: "Before you unveil your transformed movie character during your dinner date, I challenge you to elevate the dining experience by decorating your partners' plate using the food items provided. Experiment with patterns, shapes, and designs, turning your partners' dinner plate into a work of art! Make it meaningfull!",
+    prompt_2: "Your side-kick mission is to prepare a cocktail for your partner during your special dinner date. But here's the kicker – your cocktail must be more than just a drink. It should be a reflection of your creativity and love! Whether you opt for an alcoholic concoction or a refreshing mocktail, roll up your sleeves, grab your shaker, and get ready to mix up something magical!",
+    exp: 20
+  },
+  {
+    title: "Spa Staycation",
+    details: "Get ready to transform your home into a cozy spa haven. Set the mood with candles and soothing tunes, then indulge in DIY spa treatments with your partner. Enjoy massages, mindfulness (if you're into it), and healthy snacks. Very important: unplug from screens and relish that quality time together!",
+    difficulty: 1,
+    estimated_time: 2,
+    prompt_1: "It's time to treat yourselfs to some well-deserved self-care with our'Face Mask Fiesta' challenge! Your task is to either make or buy a face mask that suits your skins' needs and indulge in a rejuvenating pampering session! Don't miss the opportunity to take photos!",
+    prompt_2: "Surprise your partner with an organic tea or smothie. Indulge in this special treat and let it be a reminder of the love and care you share.",
+    exp: 10
+  },
+  {
+    title: "Compliment Showdown",
+    details: "Set a timer for 5min and take turns showering each other with the most eccentric compliments imaginable. From praising their impeccable sock matching skills to admiring their ability to sing allong weird lyrics, let your creativity run wild and see who can come up with the funniest affirmations! Prepare yourself, this challenge will have a twist.",
+    difficulty: 1,
+    estimated_time: 1,
+    prompt_1: "Your mission before the challenge start is to put together an outfit that is as wacky as possible. Raid your wardrobe for the most mismatched, colorful, and eccentric pieces you can find! Make it hard for your partner to keep a straight expression!",
+    prompt_2: "Your mission before the challenge starts is to create an eye-catching makeup look. Think flashy lipstick, vibrant eyeliner, and maybe even a faux beard for a playful twist. Get ready to bring laughter to the showdown with your bold style!",
+    exp: 10
+  },
+  {
+    title: "Surprise factor",
+    details: "Each of you will receive different challenge details on how to surprise your partner. At the end of the week, scores should be shared to see who's the champion. But here's the catch: your score will only be valid if your can make it a real surprise. If not: you should try again!",
+    difficulty: 2,
+    estimated_time: 3,
+    prompt_1: "Hi, you kisser. Its time to unleash your creativity in the realm of kisses. From sweet pecks to bold smooches, from quick bursts to lingering embraces, let your imagination run wild! Throughout the week, surprise your partner with an array of funny kiss-types. Keep track of your numbers and, at the end of the week, reveal your tally!",
+    prompt_2: "This week, get ready to astonish your partner with an array of diverse hugs! From warm bear hugs to playful twirls and every hug in between, let your creativity shine. Remember, no two hugs should be the same type! Keep track of your hugs secretly, and at the end of the week, reveal your tally.",
+    exp: 20
+  },
+  {
+    title: "Love Coupons",
+    details: "Brainstorm together and create five coupons for each of you that revolve around simple, enjoyable activities involving physical touch. These could include a foot rub station, a cuddle corner with blankets and pillows, a hand-holding stroll or any other ideas you fancy. Throughout the week, surprise each other by presenting these coupons and asking for their redemption!",
+    difficulty: 3,
+    estimated_time: 3,
+    prompt_1: "Craft a special secret coupon that fulfills one of your partner's desires or wishes, ensuring it's visually stunning and uniquely personalized. Then, present it to your partner as an extra-coupon surprise!",
+    prompt_2: "Craft a special secret coupon that fulfills one of your partner's desires or wishes, ensuring it's visually stunning and uniquely personalized. Then, present it to your partner as an extra-coupon surprise!",
+    exp: 30
+  }
+]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require_relative 'couple_challenges_data'
+require_relative 'individual_challenges_data'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +9,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+CoupleChallenge.destroy_all
+IndividualChallenge.destroy_all
+
+puts "Creating couple challenges..."
+COUPLE_CHALLENGES.each do |challenge|
+  CoupleChallenge.create!(challenge)
+end
+
+puts "Creating individual challenges..."
+INDIVIDUAL_CHALLENGES.each do |challenge|
+  IndividualChallenge.create!(challenge)
+end
+
+puts "Finished!"
