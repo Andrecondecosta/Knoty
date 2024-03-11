c373f21ef8ed09242e6c116b38426214e147020f
Ahmed Bouguacha <ahmad.bougacha@gmail.com>
2024-03-11 17:30:12 +0100

    Updated the In progress tabs of Quest log

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,9 +26,10 @@ class PagesController < ApplicationController
   end
 
   def set_pending_tasks
-    return unless current_user
+    return unless signed_in?
 
     @couple = current_user.couple_as_partner_1 || current_user.couple_as_partner_2
-    @pending_tasks = @couple.couple_tasks.where(active: false).select { |task| task.invited_id == current_user.id }
+    @pending_tasks_notif = @couple.couple_tasks.where(active: false).select { |task| task.invited_id == current_user.id }
+    @pending_tasks = @couple.couple_tasks.where(active: false)
   end
 end
