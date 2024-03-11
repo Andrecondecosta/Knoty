56fa7bd42b6eb7f942a4d1733e57c7407e316536
Andre Costa <andrecondecosta@gmail.com>
2024-03-11 22:35:38 +0000

    create a chart

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,6 +13,8 @@ class PagesController < ApplicationController
 
   def profile
     @partner = current_user == @couple.partner_1 ? @couple.partner_2 : @couple.partner_1
+    @my_love_language = set_love_language(current_user.love_language) if current_user.love_language
+    @partner_love_language = set_love_language(@partner.love_language) if @partner.love_language
   end
 
   private
@@ -22,7 +24,14 @@ class PagesController < ApplicationController
   end
 
   def set_pending_tasks
+    return unless signed_in?
+
     @couple = current_user.couple_as_partner_1 || current_user.couple_as_partner_2
     @pending_tasks = @couple.couple_tasks.where(active: false).select { |task| task.invited_id == current_user.id }
+  end
+
+  def set_love_language(l_l_result)
+
+    {"Acts of service": l_l_result.acts_of_service, "Words of affirmation": l_l_result.words_of_affirmation, "Receiving gifts": l_l_result.receiving_gifts, "Quality time": l_l_result.quality_time, "Physical touch": l_l_result.physical_touch }
   end
 end
