ae49f5bf25b20bb226560a17e668d339183b5fc8
Ahmed Bouguacha <ahmad.bougacha@gmail.com>
2024-03-14 19:45:36 +0100

    changed default url for invitations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :set_mailer_host
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name nickname birth_date gender
                                                                 avatar_url])
+  end
+
+  private
+
+  def set_mailer_host
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
   end
 end
