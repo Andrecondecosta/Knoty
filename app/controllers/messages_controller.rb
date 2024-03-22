274ca05b7e7f161eac806cb2f69e0abd5a53957a
Ana Madeira <ana_madeira1988@outlook.com>
2024-03-22 18:02:12 +0000

    almost finished chat front end

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
@@ -7,12 +8,19 @@ class MessagesController < ApplicationController
     if @message.save
       ChatroomChannel.broadcast_to(
         @chatroom,
-        render_to_string(partial: "message", locals: {message: @message})
+        message: render_to_string(partial: "message", locals: { message: @message }),
+        date: @message.created_at.strftime("%B %e"),
+        sender_id: @message.user.id
       )
       head :ok
     else
       render "chatrooms/show", status: :unprocessable_entity
     end
+  end
+
+  def show
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @messages = @chatroom.messages.order(created_at: :asc)
   end
 
   private
