68324fd06aa88f42e68eb01fb33b138bbcbf5fe9
Ahmed Bouguacha <ahmad.bougacha@gmail.com>
2024-03-23 19:58:41 +0100

    Fixed appended messages dates and avatars

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,6 @@
 class ChatroomsController < ApplicationController
   before_action :set_notifications_to_read, only: :show
+  before_action :set_partner, only: :show
 
   def show
 
@@ -12,5 +13,11 @@ class ChatroomsController < ApplicationController
   def set_notifications_to_read
     notifications = current_user.notifications.unread
     notifications.update_all(read_at: Time.zone.now)
+  end
+
+  def set_partner
+    return unless signed_in?
+
+    @partner = @couple.partner_1 == current_user ? @couple.partner_2 : @couple.partner_1
   end
 end
--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -2,14 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
-  static values = { chatroomId: Number, currentUserId: Number }
+  static values = { chatroomId: Number, currentUserId: Number, currentUserAvatarUrl: String, partnerAvatarUrl: String, lastMessageDate: String }
   static targets = ["messages"]
 
+  lastMessage = new Date(this.lastMessageDateValue)
+
   connect() {
+    const today = new Date().toLocaleDateString('en-US', { month: 'long', day: 'numeric' })
+    this.insertDate(today)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
     this.subscription = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.insertMessageOrDate(data) } // Updated callback function
+      {
+        received: data => {
+          this.insertDate(data.date)
+          this.insertMessage(data.message, data.sender_id)
+        }
+      }
     )
   }
 
@@ -24,46 +33,47 @@ export default class extends Controller {
 
   // private methods
 
-  insertMessageOrDate(data) { // Updated method name
-    if (data.message) {
-      this.insertMessage(data.message, data.sender_id)
-    } else if (data.date) {
-      this.insertDate(data.date)
-    }
-  }
-
-  insertMessage(message, senderId) { // New method to insert message
+  insertMessage(message, senderId) {
     const currentUserIsSender = this.currentUserIdValue === senderId
     const messageElement = this.buildMessageElement(currentUserIsSender, message)
     this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
     this.scrollToBottom()
   }
 
-  insertDate(date) { // New method to insert date
-    const dateElement = this.buildDateElement(date)
-    this.messagesTarget.insertAdjacentHTML("beforebegin", dateElement)
-  }
-
-  scrollToBottom() { // New method to scroll to bottom
+  scrollToBottom() {
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 
   buildMessageElement(currentUserIsSender, message) {
+    const avatar = currentUserIsSender ? this.currentUserAvatarUrlValue : this.partnerAvatarUrlValue;
+    const avatarMargin = currentUserIsSender ? 'margin-left:10px;' : 'margin-right:10px;'
+    let receiverImageElem = '';
+    let senderImageElem = ''
+    const imageElem = `
+    <img src="${avatar}" alt="avatar" style="width: 30px; height: 30px; border-radius: 50%; box-shadow: 0 0 5px rgba(0, 0, 0, 0.5); ${avatarMargin}"> `
+    currentUserIsSender ? senderImageElem = imageElem : receiverImageElem = imageElem;
+
     return `
-      <div class="message-row d-flex ${this.justifyClass(currentUserIsSender)}">
-        <div class="${this.userStyleClass(currentUserIsSender)}">
-          ${message}
-        </div>
+    <div class="message-row d-flex ${this.justifyClass(currentUserIsSender)}">
+      ${receiverImageElem}
+      <div class="${this.userStyleClass(currentUserIsSender)}">
+        ${message}
       </div>
+      ${senderImageElem}
+    </div>
     `;
   }
 
-  buildDateElement(date) { // New method to build date element
-    return `
+  insertDate(messageDate) {
+    const currentMessageDate = new Date(messageDate).toLocaleDateString('en-US', { month: 'long', day: 'numeric' })
+    const dateElem = `
       <div class="common-header">
-        ${date}
+        ${currentMessageDate}
       </div>
     `;
+
+    if (this.lastMessageDateValue.length === 0 || currentMessageDate > this.lastMessage)
+      return this.messagesTarget.insertAdjacentHTML('beforeend', dateElem)
   }
 
   justifyClass(currentUserIsSender) {
