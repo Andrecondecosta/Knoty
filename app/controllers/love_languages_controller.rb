2ef18fc59161c2cbe5693542b23607e5d46c7a47
Andre Costa <andrecondecosta@gmail.com>
2024-03-28 01:51:21 +0000

    changes profile pages

--- app/controllers/love_languages_controller.rb
+++ app/controllers/love_languages_controller.rb
@@ -2,26 +2,32 @@ require_relative '../../db/love_language_questionnaire'
 
 class LoveLanguagesController < ApplicationController
   def new
-    redirect_to home_path if current_user.love_language         # ==========> Set in PUNDIT
+    # redirect_to home_path if current_user.love_language         # ==========> Set in PUNDIT
     @love_language_questionnaire = LOVE_LANGUAGE_QUESTIONNAIRE_SHORT
     @number_of_questions = @love_language_questionnaire.size
     @love_language = LoveLanguage.new
   end
-
   def create
-    redirect_to root_page if current_user.love_language    # ==========> Set in PUNDIT
-    @love_language = LoveLanguage.new(love_language_params)
-    @love_language.user = current_user
-    if @love_language.save!
-      redirect_to profile_path, notice: "Your love language has been saved!"
+    # redirect_to home_path if current_user.love_language    # ==========> Set in PUNDIT
+    if current_user.love_language
+      LoveLanguage.update(love_language_params)
+      redirect_to profile_path, notice: "Your love language has been updated!"
     else
-      render :new, status: :unprocessable_entity
+      @love_language = LoveLanguage.new(love_language_params)
+      @love_language.user = current_user
+      if @love_language.save!
+        redirect_to profile_path, notice: "Your love language has been saved!"
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
   end
-
   private
-
   def love_language_params
-    params.require(:love_language).permit(:acts_of_service, :words_of_affirmation, :receiving_gifts, :quality_time, :physical_touch)
+    params.require(:love_language).permit(:acts_of_service,
+                                          :words_of_affirmation,
+                                          :receiving_gifts,
+                                          :quality_time,
+                                          :physical_touch)
   end
 end
